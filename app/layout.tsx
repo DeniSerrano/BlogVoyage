0b45d8a6697d962847e9009728075ef4700ff6cd
Denis Serrano <82126494+DeniSerrano@users.noreply.github.com>
2026-03-01 00:37:00 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" />
-      </head>
       <body>
         {children}
       </body>
