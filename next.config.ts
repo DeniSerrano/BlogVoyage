d94feb243703457ab81ec86188bb56d31a8fe799
Denis Serrano <82126494+DeniSerrano@users.noreply.github.com>
2026-03-01 00:27:17 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@tiendanube/components'],
-  // Forzamos a que no use Turbopack si el entorno es producción
-  turbo: {
-    rules: {
-      "*.css": ["css-loader"],
-    },
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
